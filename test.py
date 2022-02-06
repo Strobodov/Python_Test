b9683470d8c6f95180413e11c4c6c97f8985a7f6
Marcel Kornegoor <m.kornegoor@atcomputing.nl>
2022-02-06 18:25:29 +0100

    Eerste regels code!

---
+++ test.py
@@ -0,0 +1,9 @@
+def my_function():
+    username = input("Enter username:")
+    k = 10
+    while (k < 14):
+        print("Hello World!")
+        print(username)
+        k += 1
+
+my_function(pet)
